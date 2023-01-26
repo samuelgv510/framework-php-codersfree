7f4801c0c935a762ebc8dc9bdfa56af9372f8996
Samuel Guerrero <samuelg510@gmail.com>
2023-01-25 21:15:58 -0500

    Simple Enrutador con PHP

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Lib\Route;
+
+Route::get('/', function () {
+    echo 'hola desde la página principal';
+});
+
+Route::get('/contact', function () {
+    echo 'hola desde la página de contacto';
+});
+
+Route::get('/contact', function () {
+    echo 'hola desde la página acerca de';
+});

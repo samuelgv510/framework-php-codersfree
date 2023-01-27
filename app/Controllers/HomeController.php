defa5a5c0c7c320a2d100eb9bf498d78523953ea
Samuel Guerrero <samuelg510@gmail.com>
2023-01-27 00:31:33 -0500

    Conexión a una BBDD MySQL

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,14 +2,16 @@
 
 namespace App\Controllers;
 
+use App\Models\Contact;
+
 class HomeController extends Controller
 {
     public function index()
     {
+        $contactModel = new  Contact();
         return $this->view('home', [
             'title' => 'Home',
             'description' => "Esta es la página home"
         ]);
-        //return $this->view('prueba.test');
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
 
+require_once '../config/database.php';
 require_once '../autoload.php';
 require_once '../routes/web.php';

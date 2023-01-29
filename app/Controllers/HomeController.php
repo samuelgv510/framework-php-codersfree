c0afc3aefbfe1b5f069e75c57c1e7c6df6bfb6d9
Samuel Guerrero <samuelg510@gmail.com>
2023-01-29 09:13:43 -0500

    Cómo hacer consultas SQL a una BBDD MySQL

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -9,6 +9,8 @@ class HomeController extends Controller
     public function index()
     {
         $contactModel = new  Contact();
+        return $contactModel->query('SELECT * FROM contacts')->get();
+
         return $this->view('home', [
             'title' => 'Home',
             'description' => "Esta es la página home"
